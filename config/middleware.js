12efeb012756c499b33c2d1f2934e6855850dc01
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2021-02-11 18:58:14 +0530

    upgraded strapi version

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,14 @@
 module.exports = {
+  load: {
+    before: ["responseTime", "boom", "logger", "cors", "responses", "gzip"],
+    after: ["parser", "router"],
+  },
   settings: {
     gzip: {
       enabled: true,
+      options: {
+        br: false,
+      },
     },
   },
 };
